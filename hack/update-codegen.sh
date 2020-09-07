c2581eaa22a3011b61e333c9c1a21a7f2ee38f7d
Sebastian Stauch <sebastian.stauch@sap.com>
2020-09-07 15:21:42 +0200

    update go version and add debug logging

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 rm -f $GOPATH/bin/*-gen
 
 PROJECT_ROOT=$(dirname $0)/..
