f11ddc48d92569aed46164c97b39881d870176d1
Dominik Froehlich <dominik.froehlich@sap.com>
2025-06-06 09:11:39 +0200

    fix: remove sleeps

--- test/e2e/network_connectivity_test.go
+++ test/e2e/network_connectivity_test.go
@@ -7,21 +7,20 @@ package e2e_test
 import (
 	"context"
 	"path/filepath"
-	"time"
 
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	"github.com/gardener/gardener/test/utils/access"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gardener/gardener-extension-networking-cilium/test/templates"
-	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
-	"github.com/gardener/gardener/test/utils/access"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("Network Extension Tests", Label("Network"), func() {
@@ -75,38 +74,39 @@ var _ = Describe("Network Extension Tests", Label("Network"), func() {
 		Expect(err).NotTo(HaveOccurred())
 		f.TemplatesDir = filepath.Join(resourceDir, "templates")
 
+		By("Deploy network-test Job")
 		err = f.RenderAndDeployTemplate(ctx, f.ShootFramework.ShootClient, templates.NetworkConnectivityTestJobName, values)
 		Expect(err).NotTo(HaveOccurred())
-		time.Sleep(30 * time.Second)
-		err = f.ShootFramework.WaitUntilPodIsRunningWithLabels(
-			ctx,
-			labels.SelectorFromSet(labels.Set{v1beta1constants.LabelApp: "networking-test"}),
-			templates.NetworkConnectivityTestNamespace,
-			f.ShootFramework.ShootClient,
-		)
-		Expect(err).NotTo(HaveOccurred())
-		By("Network-test Job was deployed successfully!")
+		Eventually(func() error {
+			err = f.ShootFramework.WaitUntilPodIsRunningWithLabels(
+				ctx,
+				labels.SelectorFromSet(labels.Set{v1beta1constants.LabelApp: "networking-test"}),
+				templates.NetworkConnectivityTestNamespace,
+				f.ShootFramework.ShootClient,
+			)
+			return err
+		}).WithTimeout(defaultTimeout).Should(Succeed())
 
 		By("Check if network-test Job fails or succeeds!")
 
 		job := &batchv1.Job{}
 		var succeeded bool
-		for {
+		Eventually(func() bool {
+			keepGoing := true
 			err = f.ShootFramework.ShootClient.Client().Get(ctx, client.ObjectKey{Namespace: values.HelmDeployNamespace, Name: "network-test"}, job)
 			Expect(err).NotTo(HaveOccurred())
 
 			if job.Status.Succeeded > 0 {
 				succeeded = true
-				break
+				keepGoing = false
 			}
 
 			if job.Status.Failed > 0 {
 				succeeded = false
-				break
+				keepGoing = false
 			}
-
-			time.Sleep(1 * time.Second)
-		}
+			return keepGoing
+		}).WithTimeout(defaultTimeout).Should(BeFalse())
 
 		By("Dump networking-test logs!")
 		err = f.ShootFramework.DumpLogsForPodsWithLabelsInNamespace(ctx, f.ShootFramework.ShootClient, values.HelmDeployNamespace, client.MatchingLabels{v1beta1constants.LabelApp: "networking-test"})
