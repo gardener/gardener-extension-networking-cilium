7944e58e80c436f3f8b5948832451bf626737d47
Johannes Scheerer <johannes.scheerer@sap.com>
2025-06-27 10:53:03 +0200

    Add validation to admission controller

---
+++ pkg/admission/validator/shoot.go
@@ -0,0 +1,112 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package validator
+
+import (
+	"context"
+	"fmt"
+
+	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
+	"github.com/gardener/gardener/pkg/apis/core"
+	gardencorehelper "github.com/gardener/gardener/pkg/apis/core/helper"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/gardener/gardener-extension-networking-cilium/pkg/apis/cilium"
+	"github.com/gardener/gardener-extension-networking-cilium/pkg/apis/cilium/v1alpha1"
+	ciliumvalidation "github.com/gardener/gardener-extension-networking-cilium/pkg/apis/cilium/validation"
+	"github.com/gardener/gardener-extension-networking-cilium/pkg/controller"
+)
+
+// NewShootValidator returns a new instance of a shoot validator.
+func NewShootValidator(mgr manager.Manager) extensionswebhook.Validator {
+	return &shoot{
+		client:         mgr.GetClient(),
+		decoder:        serializer.NewCodecFactory(mgr.GetScheme(), serializer.EnableStrict).UniversalDecoder(),
+		lenientDecoder: serializer.NewCodecFactory(mgr.GetScheme()).UniversalDecoder(),
+	}
+}
+
+func NewValidator() extensionswebhook.Validator {
+	return &shoot{}
+}
+
+type shoot struct {
+	client         client.Client
+	decoder        runtime.Decoder
+	lenientDecoder runtime.Decoder
+}
+
+// Validate validates the given shoot object.
+func (s *shoot) Validate(ctx context.Context, newObj, old client.Object) error {
+	shoot, ok := newObj.(*core.Shoot)
+	if !ok {
+		return fmt.Errorf("wrong object type %T", newObj)
+	}
+
+	// Skip if it's a workerless Shoot
+	if gardencorehelper.IsWorkerless(shoot) {
+		return nil
+	}
+
+	shootV1Beta1 := &gardencorev1beta1.Shoot{}
+	err := gardencorev1beta1.Convert_core_Shoot_To_v1beta1_Shoot(shoot, shootV1Beta1, nil)
+	if err != nil {
+		return err
+	}
+
+	if old != nil {
+		oldShoot, ok := old.(*core.Shoot)
+		if !ok {
+			return fmt.Errorf("wrong object type %T for old object", old)
+		}
+		return s.validateShootUpdate(ctx, oldShoot, shoot)
+	}
+
+	return s.validateShootCreation(ctx, shoot)
+}
+
+func (s *shoot) validateShoot(_ context.Context, shoot *core.Shoot) error {
+	// Network validation
+	if shoot.Spec.Networking != nil {
+		if errList := ciliumvalidation.ValidateNetworking(shoot.Spec.Networking, field.NewPath("spec", "networking")); len(errList) != 0 {
+			return errList.ToAggregate()
+		}
+
+		if shoot.Spec.Networking.ProviderConfig != nil {
+			network := &extensionsv1alpha1.Network{}
+			network.Spec.ProviderConfig = shoot.Spec.Networking.ProviderConfig
+			networkConfig, err := controller.CiliumNetworkConfigFromNetworkResource(network)
+			if err != nil {
+				return err
+			}
+
+			internalNetworkConfig := &cilium.NetworkConfig{}
+			err = v1alpha1.Convert_v1alpha1_NetworkConfig_To_cilium_NetworkConfig(networkConfig, internalNetworkConfig, nil)
+			if err != nil {
+				return err
+			}
+
+			if errList := ciliumvalidation.ValidateNetworkConfig(internalNetworkConfig, field.NewPath("spec", "networking", "providerConfig")); len(errList) != 0 {
+				return errList.ToAggregate()
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *shoot) validateShootUpdate(ctx context.Context, oldShoot, shoot *core.Shoot) error {
+	return s.validateShoot(ctx, shoot)
+}
+
+func (s *shoot) validateShootCreation(ctx context.Context, shoot *core.Shoot) error {
+	return s.validateShoot(ctx, shoot)
+}
