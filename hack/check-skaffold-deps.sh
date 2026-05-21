cb662f0afd9e15b9fcf171ddccdb985583f27919
Dominik Froehlich <dominik.froehlich@sap.com>
2026-05-21 18:01:35 +0200

    chore: switch to gardener-operator

---
+++ hack/check-skaffold-deps.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+operation="${1:-check}"
+
+echo "> ${operation} Skaffold Dependencies"
+
+success=true
+
+function run() {
+  if ! bash "$GARDENER_HACK_DIR"/check-skaffold-deps-for-binary.sh "$operation" --skaffold-file "$1" --binary "$2" --skaffold-config "$3"; then
+    success=false
+  fi
+}
+
+# skaffold.yaml
+run "skaffold.yaml" "gardener-extension-networking-cilium" "networking-cilium"
+
+if ! $success ; then
+  exit 1
+fi
